783813c9251b76a90f7c753262b1925bc06ec3be
men_l <men_long@yahoo.com>
2024-10-23 11:18:13 -0400

    update

--- src/config.ts
+++ src/config.ts
@@ -2,8 +2,7 @@ export default {
   botName: "BookTripUsingSDK",
   botVersion: "1",
   region: "us-east-1",
-  roleArn:
-    "arn:aws:iam::123456789012:role/aws-service-role/lexv2.amazonaws.com/AWSServiceRoleForLexV2Bots_ABCDEFGHIJK",
+  roleArn: "arn:aws:iam::471112503258:role/aws-service-role/lexv2.amazonaws.com/AWSServiceRoleForLexV2Bots_ABCDEFGHIJK",
   TTLInSeconds: 1800,
   locale: "en_US",
 }
